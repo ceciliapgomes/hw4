452a152807ace044f8c5d1cfa1d2ea23986a22a8
Cecilia Gomes <cecilia.gomes@kellogg.northwestern.edu>
2024-06-04 19:15:15 +0000

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def current_user
     puts "------------------ code before every request ------------------"
   end
+
+  helper_method :current_user
+
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
 end
